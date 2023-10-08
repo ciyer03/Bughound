c3d0b18e3f94f0cb52a31d3b61aaf0b2bb3e78cd
shannonluu2003 <shannonluu2003@gmail.com>
2023-10-07 18:47:02 -0700

    Add files via upload
    shannon's workspace

---
+++ bughound/Navigation/src/application/controller/MainController.java
@@ -0,0 +1,31 @@
+package application.controller;
+
+import java.io.IOException;
+import java.net.URL;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+
+public class MainController {
+	
+	@FXML HBox mainBox;
+
+	@FXML public void createProjectOp() {
+		
+		URL url = getClass().getClassLoader().getResource("view/Project_Page.fxml");
+		
+		try {
+			AnchorPane pane1 = (AnchorPane) FXMLLoader.load(url);
+			
+			mainBox.getChildren().add(pane1);
+			
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
